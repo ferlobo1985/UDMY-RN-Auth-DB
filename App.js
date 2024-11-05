3444276181712cb0cf09733879f23aae95cf4646
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-05 05:42:20 -0300

    06-Sign out user
    06-Sign out user

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { 
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList
+ } from "@react-navigation/drawer";
 
 /// firebase
 import { onAuthStateChanged } from "firebase/auth";
@@ -8,19 +13,40 @@ import { AUTH } from "./firebaseConfig";
 // SCREENS
 import Home from './screens/home';
 import SignIn from './screens/signIn';
+import { useState } from "react";
 
 const Drawer = createDrawerNavigator();
+
+function CustomDrawerContent(props){
+  return(
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props}/>
+      { props.user &&
+        <DrawerItem label="Sign out" onPress={()=>AUTH.signOut()}/>
+      }
+    </DrawerContentScrollView>
+  )
+}
+
 export default function App() {
+  const [user,setUser] = useState(null)
 
   onAuthStateChanged(AUTH,(user)=>{
-    console.log(user)
+    if(user){ setUser(user) }
+    else { setUser(null)}
   })
 
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
+      <Drawer.Navigator
+        drawerContent={(props)=>
+          <CustomDrawerContent {...props} user={user}/>
+        }
+      >
         <Drawer.Screen name="Home" component={Home}/>
-        <Drawer.Screen name="Auth" component={SignIn}/>
+        { !user &&
+          <Drawer.Screen name="Auth" component={SignIn}/>
+        }
       </Drawer.Navigator>
     </NavigationContainer>
   );
