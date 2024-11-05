7fd905acbac34727f43b07fbadd01c76ce3656a6
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-05 03:37:26 -0300

    02-Firebase Set Up
    02-Firebase Set Up

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA3iuDlkSsOzZBzeDnR-avUtX-T27Vb9Dc",
+    authDomain: "rn-auth-a0be4.firebaseapp.com",
+    projectId: "rn-auth-a0be4",
+    storageBucket: "rn-auth-a0be4.firebasestorage.app",
+    messagingSenderId: "294330837985",
+    appId: "1:294330837985:web:5704b968cf58d60d37c0a0",
+    measurementId: "G-3H1T7V6Z56"
+};
+
+const app = initializeApp(firebaseConfig)
